49d94d06767e23cf0f63792a3616aecebe0b13fb
SangWon7242 <gotkdqja@gmail.com>
2025-05-27 20:26:40 +0900

    INSERT 테스트에서 Sql 객체를 사용하여 insert 메서드 실행

--- src/test/java/com/sbs/simpleDb/SimpleDbTest.java
+++ src/test/java/com/sbs/simpleDb/SimpleDbTest.java
@@ -1,8 +1,10 @@
 package com.sbs.simpleDb;
 
 import com.sbs.global.simpleDb.SimpleDb;
+import com.sbs.global.simpleDb.Sql;
 import org.junit.jupiter.api.*;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 
 @TestInstance(PER_CLASS)
@@ -35,10 +37,10 @@ public class SimpleDbTest {
   @Test
   @DisplayName("INSERT 테스트")
   public void t1() {
-    int no = 1;
-    String subject = "제목 %d".formatted(no);
-    String content = "내용 %d".formatted(no);
+    String subject = "제목 new";
+    String content = "내용 new";
 
+    /*
     simpleDb.run("""
         INSERT INTO article
         SET createDate = NOW(),
@@ -46,5 +48,21 @@ public class SimpleDbTest {
         subject = ?,
         content = ?;
         """, subject, content);
+     */
+
+    Sql sql = simpleDb.genSql();
+    sql.append("INSERT INTO article");
+    sql.append("SET createDate = NOW()");
+    sql.append(", modifiedDate = NOW()");
+    sql.append(", subject = ?", subject);
+    sql.append(", content = ?", content);
+
+    long newId = sql.insert(); // AUTO_INCREMENT에 의해 생성된 주키 리턴
+
+    // 들어온 값이 0보다 크면 성공, 0보다 작으면 실패
+    assertThat(newId).isGreaterThan(0);
+    
+    // 들어온 값이 1이랑 같냐
+    assertThat(newId).isEqualTo(1);
   }
 }
