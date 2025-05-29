8790754ee9f52ded0b0f7ddac4e2649d8271320f
SangWon7242 <gotkdqja@gmail.com>
2025-05-29 19:34:26 +0900

    게시물 리스트 역순 조회 테스트

--- src/test/java/com/sbs/simpleDb/SimpleDbTest.java
+++ src/test/java/com/sbs/simpleDb/SimpleDbTest.java
@@ -120,4 +120,33 @@ public class SimpleDbTest {
 
     assertThat(articleRows.size()).isEqualTo(5);
   }
+
+  @Test
+  @DisplayName("SELECT 테스트 2 : 역순 조회")
+  public void t3() {
+    Sql sql = simpleDb.genSql();
+
+    sql.append("SELECT *");
+    sql.append("FROM article");
+    sql.append("ORDER BY id DESC");
+
+    // 데이터가 2차원으로 날라옴
+    List<Map<String, Object>> articleRows = sql.selectRows();
+
+    // 정순 체크
+    IntStream.range(0, articleRows.size()).forEach(i -> {
+      long id = articleRows.size() - i;
+      Map<String, Object> articleRow = articleRows.get(i);
+
+      assertThat(articleRow.get("id")).isEqualTo(id);
+      assertThat(articleRow.get("createDate")).isInstanceOf(LocalDateTime.class);
+      assertThat(articleRow.get("createDate")).isNotNull();
+      assertThat(articleRow.get("modifiedDate")).isInstanceOf(LocalDateTime.class);
+      assertThat(articleRow.get("modifiedDate")).isNotNull();
+      assertThat(articleRow.get("subject")).isEqualTo("제목 %d". formatted(id));
+      assertThat(articleRow.get("content")).isEqualTo("내용 %d". formatted(id));
+    });
+
+    assertThat(articleRows.size()).isEqualTo(5);
+  }
 }
